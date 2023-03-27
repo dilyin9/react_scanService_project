928877cd121f330b9ed171b6fe0232249da3ea5b
dilyin9 <dilyin9@gmail.com>
2023-03-28 01:13:50 +0300

    44

--- scan-service/src/app/Components/Layout/Navbar/Navbar.jsx
+++ scan-service/src/app/Components/Layout/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import LabelIcon from '../../../Assets/label.png'
+import styles from './navbar.module.scss'
 const Navbar = () => {
   return (
     <div className="app-header">
       <div>
-        <img className="label-image" src={LabelIcon}/>
+        <img className={styles["label-image"]} src={LabelIcon}/>
+      </div>
+      <div>
+        <nav>
+          <li>Главная</li>
+          <li>Тарифы</li>
+          <li>FAQ</li>
+        </nav>
       </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
